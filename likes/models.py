53911263e295278251c31bd079e9afb10431189c
Samyak Jain <samyak65400@gmail.com>
2025-08-31 19:57:16 +0530

    minor fixes from past

--- likes/models.py
+++ likes/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.auth.models import User
 
 
 class LikedItem(models.Model):
